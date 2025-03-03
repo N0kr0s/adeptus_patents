12db31ef0e11da57891a4de5f49b7cfe9e276f09
N0kr0s <ka0n5206@gmail.com>
2025-03-04 00:22:52 +0300

    update file_path

--- script/parcer.py
+++ script/parcer.py
@@ -1,6 +1,6 @@
 from data_processing import process_excel_and_save
 
-excel_file_path = '../data/gp-search-20250301-010925.xlsx'
-csv_file_path = '../data/patents.csv'
+excel_file_path = 'C:\\adeptus_data\\gp.xlsx'
+csv_file_path = 'C:\\adeptus_data\\patents.csv'
 
 process_excel_and_save(excel_file_path, csv_file_path)
