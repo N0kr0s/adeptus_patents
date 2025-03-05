10023c4b191d5b1a97c21ebac2613ef7dfada215
N0kr0s <ka0n5206@gmail.com>
2025-03-05 20:17:09 +0300

    rename py file

--- script/parcer.py
+++
@@ -1,6 +0,0 @@
-from data_processing import process_excel_and_save
-
-excel_file_path = 'C:\\adeptus_data\\gp.xlsx'
-csv_file_path = 'C:\\adeptus_data\\patents.csv'
-
-process_excel_and_save(excel_file_path, csv_file_path)
